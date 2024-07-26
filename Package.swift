fc51476a31ee4ff85907801b1fed45db91d03acd
Martin Johannesson <mjohannesson@gmail.com>
2024-07-26 15:36:51 +0200

    Remove unused code.

--- Package.swift
+++ Package.swift
@@ -10,25 +10,9 @@ let package = Package(
     products: [
         .executable(name: "mkprivacy", targets: ["mkprivacy"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apparata/SystemKit", exact: "1.7.0"),
-    ],
     targets: [
         .executableTarget(
             name: "mkprivacy",
-            dependencies: [
-                "MakePrivacyKit",
-            ],
-            swiftSettings: [
-                .define("DEBUG", .when(configuration: .debug)),
-                .define("RELEASE", .when(configuration: .release)),
-                .define("SWIFT_PACKAGE")
-            ]),
-        .target(
-            name: "MakePrivacyKit",
-            dependencies: [
-                "SystemKit"
-            ],
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug)),
                 .define("RELEASE", .when(configuration: .release)),
